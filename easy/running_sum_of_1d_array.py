45582be9fe0621954aa5dbedb99184f263ec5b1d
Vijay Benz <benzwrites@gmail.com>
2022-08-29 17:33:28 +0530

    Create running_sum_of_1d_array.py

---
+++ easy/running_sum_of_1d_array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        sum = 0
+        res = []
+        for i in nums:
+            sum+=i
+            res.append(sum)
+        return res
