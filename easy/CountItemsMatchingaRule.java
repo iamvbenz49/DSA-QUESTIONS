e53f58c4aeda096232ad036b10238c153af23ce6
Sham Vijay <benzwrites@gmail.com>
2023-01-17 18:29:21 +0530

    Create CountItemsMatchingaRule.java

---
+++ easy/CountItemsMatchingaRule.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int i = -1;
+        if(ruleKey.equals("type")){
+            i=0;
+        }
+        else if(ruleKey.equals("color")){
+            i=1;
+        }
+        else if(ruleKey.equals("name")){
+            i=2;
+        }
+        int count =0;
+        for(int j=0;j<items.size();j++){
+            if(items.get(j).get(i).equals(ruleValue)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
