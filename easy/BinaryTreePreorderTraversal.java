b38ddf84e5ff8b0a5bc644afebbf042dabd7284c
Sham Vijay <benzwrites@gmail.com>
2023-01-24 18:34:03 +0530

    Update BinaryTreePreorderTraversal.java

--- easy/BinaryTreePreorderTraversal.java
+++ easy/BinaryTreePreorderTraversal.java
@@ -13,6 +13,7 @@
  *     }
  * }
  */  
+                    //USING RECURSION
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<Integer>(); 
@@ -25,5 +26,37 @@ class Solution {
         if(root.left!=null) traversal(root.left,list);
         
         if(root.right!=null) traversal(root.right,list);
+    }
+}
+
+            //USING ITERATION
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        if(root==null)return list;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            root = stack.pop();
+            list.add(root.val);
+            if(root.right!=null)stack.push(root.right);
+            if(root.left!=null)stack.push(root.left);
+        }
+        return list;
     }
 }
