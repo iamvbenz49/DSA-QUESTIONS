d668dac881eb19b9a07f03729f5fef050c7fd631
Sham Vijay <benzwrites@gmail.com>
2023-02-06 18:27:06 +0530

    Create BestTimetoBuyandSellStock.java

---
+++ BestTimetoBuyandSellStock.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int l = 0;
+        int r =1;
+        int maxProf = 0;
+        while(r<prices.length){
+            int currProf = prices[r]-prices[l];
+            if(prices[l]<prices[r])
+                maxProf = Math.max(currProf,maxProf);
+            else
+                l = r;
+            r++;
+        }
+        return maxProf;
+    }
+} //pattern used SLIDING WINDOW
