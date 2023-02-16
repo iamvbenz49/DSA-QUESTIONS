57edee4fc4d4e0ebd7cc3648374b496adb3df787
Sham Vijay <benzwrites@gmail.com>
2023-02-16 18:12:23 +0530

    Update maximumDepthOfBinaryTree.java

--- easy/maximumDepthOfBinaryTree.java
+++ easy/maximumDepthOfBinaryTree.java
@@ -24,3 +24,13 @@ class Solution {
         return 1+Math.max(depth(root.left),depth(root.right));
     }
 }
+//less-code
+class Solution {
+    public int maxDepth(TreeNode root) {
+        return depth(root);
+    }
+    public int depth(TreeNode root){
+        if(root==null)return 0;
+        return 1+Math.max(depth(root.left),depth(root.right));
+    }
+}
