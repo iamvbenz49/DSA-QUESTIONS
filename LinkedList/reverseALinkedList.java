0f4f02191c61bc3b4cb0648a5717c9973e6fbb29
Sham Vijay <benzwrites@gmail.com>
2023-05-14 19:11:36 +0530

    Create reverseALinkedList.java

---
+++ LinkedList/reverseALinkedList.java
@@ -0,0 +1,29 @@
+//iterative
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode next = null;
+        while(head!=null){
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
+
+//recursive
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode next = null;
+        while(head!=null){
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
