b832945ee3b467fb1beff666346abff4963c3291
Sham Vijay <benzwrites@gmail.com>
2023-06-23 20:56:50 +0530

    Create splitaStringinBalancedStrings.java

---
+++ Greedy/splitaStringinBalancedStrings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int r = 0;
+        int l = 0;
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='R'){
+                r++;
+            }
+            else{
+                l++;
+            }
+            if(l==r){
+                l=0;
+                r=0;
+                count+=1;
+            }
+        }
+        return count;
+    }
+}
