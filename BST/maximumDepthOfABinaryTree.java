3c59b39f4bb70604022f3388019f5056301b1f00
Sham Vijay <benzwrites@gmail.com>
2023-10-24 23:45:10 +0530

    Create maximumDepthOfABinaryTree.java

---
+++ BST/maximumDepthOfABinaryTree.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+        return dfs(root);
+    }
+    public int dfs(TreeNode root){
+        if(root==null)
+            return 0;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        return 1+Math.max(left,right);
+    }
+}
