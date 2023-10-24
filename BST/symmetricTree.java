35e0a20bb07d1db40608a51177324e4a4007c56c
Sham Vijay <benzwrites@gmail.com>
2023-10-24 23:43:17 +0530

    Create symmetricTree.java

---
+++ BST/symmetricTree.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetric(root.left,root.right);
+    }
+    public boolean isSymmetric(TreeNode root1,TreeNode root2){
+        if(root1==null&&root2==null)
+            return true;
+        if(root1==null||root2==null)
+            return false;
+        if(root1.val!=root2.val)
+            return false;
+        return isSymmetric(root1.left,root2.right) && isSymmetric(root1.right,root2.left);
+    }
+}
