4816c23335296d9c7d3d9d1c9d5cd17d8447bc57
Sham Vijay <benzwrites@gmail.com>
2023-10-24 23:44:35 +0530

    Create invertBinaryTree.java

---
+++ BST/invertBinaryTree.java
@@ -0,0 +1,17 @@
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        dfs(root);
+        return root;
+    }
+    public void dfs(TreeNode root){
+        if(root==null)
+            return;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        dfs(root.left);
+        dfs(root.right);
+    }
+}
