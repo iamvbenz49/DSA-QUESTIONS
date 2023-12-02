66282e5f5737cace4394a2018ca2b3edd8e56174
Sham Vijay <benzwrites@gmail.com>
2023-12-02 16:28:00 +0530

    Update BSTIterator.java

--- BST/BSTIterator.java
+++ BST/BSTIterator.java
@@ -1,3 +1,4 @@
+TC - O(N) SC - O(H)
 class BSTIterator {
     Stack<TreeNode> stack = new Stack<>();
     public BSTIterator(TreeNode root) {
@@ -19,5 +20,41 @@ class BSTIterator {
     }
     public boolean hasNext() {
         return !stack.isEmpty();
+    }
+}
+
+// TC - O(1) SC - O(N)
+class BSTIterator {
+    List<Integer> list;
+    int idx;
+    public BSTIterator(TreeNode root) {
+        list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        stack.add(root);
+        while(!stack.isEmpty()){
+            if(node!=null && node.left!=null){
+                stack.add(node.left);
+                node = node.left;
+            }else{
+                node = stack.pop();
+                list.add(node.val);
+                if(node.right!=null){
+                    stack.add(node.right);
+                    node = node.right;
+                }else{
+                    node = null;
+                }
+            }
+        }
+        idx = 0;
+    }
+    
+    public int next() {
+        return list.get(idx++);
+    }
+    
+    public boolean hasNext() {
+        return list.size()!=idx;
     }
 }
