97fc0f66926ec3a22366c01a61a91163cb0c30ce
Sham Vijay <benzwrites@gmail.com>
2023-12-13 22:24:41 +0530

    Create subsetSums.java

---
+++ Backtracking/subsetSums.java
@@ -0,0 +1,21 @@
+// TC - O(2^N) 
+// SC - O(2^N) + O(N)
+
+class Solution{
+    ArrayList<Integer> subsetSums(ArrayList<Integer> arr, int N){
+        //SC - O(2^N)
+        ArrayList<Integer> res = new ArrayList<>();
+        subsetSum(arr,res,0,0);
+        return res;
+    }
+    // TC - O(2^N) 
+    // Auxillary stack space O(N)
+    void subsetSum(ArrayList<Integer> arr,ArrayList<Integer> res,int sum,int index){
+        if(index==arr.size()){
+            res.add(sum);
+            return;
+        }
+        subsetSum(arr,res,sum+arr.get(index),index+1);
+        subsetSum(arr,res,sum,index+1);
+    }
+}
