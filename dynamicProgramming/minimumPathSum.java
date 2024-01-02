a90d03228efd5929ddcab50a1ed7546b744e16ca
Sham Vijay <benzwrites@gmail.com>
2024-01-02 13:52:59 +0530

    Update minimumPathSum.java

--- dynamicProgramming/minimumPathSum.java
+++ dynamicProgramming/minimumPathSum.java
@@ -22,7 +22,7 @@ class Solution {
         return dp[row][col] = grid[row][col] + Math.min(goRight,goDown);
     }
 }
-//TC - O(M*N) SC - O(M+N) + O(M+N)
+//TC - O(M*N) SC - O(M+N) + O(M*N)
 class Solution {
     public int minPathSum(int[][] grid) {
         int[][] dp = new int[grid.length][grid[0].length];
@@ -43,5 +43,48 @@ class Solution {
         int goRight = bruteForce(grid,dp,row,col+1);
         int goDown = bruteForce(grid,dp,row+1,col);
         return dp[row][col] = grid[row][col] + Math.min(goRight,goDown);
+    }
+}
+
+//TC - O(M*N) SC - O(M*N)
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[m-1][n-1] = grid[m-1][n-1];
+        for(int i=m-2;i>=0;i--){
+            dp[i][n-1] = grid[i][n-1] + dp[i+1][n-1];
+        }
+        for(int i=n-2;i>=0;i--){
+            dp[m-1][i] = grid[m-1][i] + dp[m-1][i+1];
+        }
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                dp[i][j] = grid[i][j] + Math.min(dp[i+1][j],dp[i][j+1]);
+            }
+        }
+        return dp[0][0];
+    }
+}
+}
+
+//TC - O(M*N) SC - O(2N)
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[] dp = new int[n];
+        dp[n-1] = grid[m-1][n-1]; 
+        for(int i=n-2;i>=0;i--){
+            dp[i] = dp[i+1] + grid[m-1][i];
+        }
+        for(int i=m-2;i>=0;i--){
+            int[] temp = new int[n];
+            temp[n-1] = dp[n-1] + grid[i][n-1];
+            for(int j=n-2;j>=0;j--){
+                temp[j] = grid[i][j] + Math.min(temp[j+1],dp[j]);
+            }
+            dp = temp;
+        }
+        return dp[0];
     }
 }
