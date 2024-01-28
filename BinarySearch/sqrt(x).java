65ca838b01fb886c815ad820e923f5eb6c6b2cb6
Sham Vijay <benzwrites@gmail.com>
2024-01-28 22:27:20 +0530

    Update sqrt(x).java

--- BinarySearch/sqrt(x).java
+++ BinarySearch/sqrt(x).java
@@ -7,7 +7,7 @@ class Solution {
         int end = x;
         while(start<=end){
             int mid = start + (end-start)/2;
-            if(x/mid==mid){
+            if(x/mid==mid){  // mid*mid = x ---> mid = x/mid
                 return mid;
             }
             else if(x/mid<mid){
