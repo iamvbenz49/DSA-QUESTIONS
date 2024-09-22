9fa54c13d342a57afa71d7e0bd5d3d949692d9a0
Sham Vijay <benzwrites@gmail.com>
2024-09-22 21:25:04 +0530

    Iterative approach for wildcard matching

--- dynamicProgramming/wildcardMatching.java
+++ dynamicProgramming/wildcardMatching.java
@@ -33,3 +33,36 @@ class Solution {
         return 0;
     }
 }
+
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        
+
+        dp[0][0] = true;
+        
+        // Handle the case where s is empty but p contains '*'
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char sChar = s.charAt(i - 1);
+                char pChar = p.charAt(j - 1);
+                
+                if (pChar == sChar || pChar == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pChar == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+}
