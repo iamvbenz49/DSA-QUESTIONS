59d52be69040b1760201de94554a1eaa00ba5490
Sham Vijay <benzwrites@gmail.com>
2024-10-07 22:55:11 +0530

    stack

---
+++ Stack/infixToPostfix.java
@@ -0,0 +1,57 @@
+class Solution {
+    // Function to convert an infix expression to a postfix expression.
+    public static  int findPrec(char c) {
+        if(c == '+' || c == '-') {
+            return 1;
+        } else if(c == '*' || c == '/') {
+            return 2;
+        } else if(c == '^') {
+            return 3;
+        } else if(c == '(') {
+            return 18;
+        }
+        return 20;
+    }
+    public static String infixToPostfix(String s) {
+        // ^ greater than * equals to / greater than + equals to -. 
+        // Ignore the right associativity of ^.
+        // Priority 1 -> ^
+        // 2 -> * /
+        // 3 -> + -
+        
+        // if letter add to string
+        // if stack empty and stack.peek precedence >= current precendence
+        // add to string
+        // if ) encountered 
+        // add all symbols to stack until )
+        // if stack.peek precedence < current precendence
+        // add to stack
+        int N = s.length();
+        Stack<Character> stack = new Stack<>();
+        String res = "";
+        for(int i = 0; i < N; i++) {
+            char c = s.charAt(i);
+            if(Character.isLetterOrDigit(c)) {
+                res += c;
+            } else {
+                if(stack.isEmpty() || (stack.peek() != '(' && findPrec(stack.peek()) >= findPrec(c))) {
+                    while(!stack.isEmpty() && stack.peek() != '(' && findPrec(stack.peek()) >= findPrec(c)) {
+                        res += stack.pop();
+                    }
+                    stack.add(c);
+                } else if(c == ')'){
+                    while(stack.peek() != '(') {
+                        res += stack.pop();
+                    }
+                    stack.pop();
+                } else {
+                    stack.add(c);
+                }
+            }
+        }
+        while(!stack.isEmpty()) {
+            res += stack.pop();
+        }
+        return res;
+    }
+}
