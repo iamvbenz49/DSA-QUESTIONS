9021726772bc5987d3ba3bfa08258a66e6201249
Sham Vijay <benzwrites@gmail.com>
2024-11-03 19:04:37 +0530

    Classical Graph problem, how did i miss this in contest :(

---
+++ Dijkstra/findMinimumTimeToReachLastRoom.java
@@ -0,0 +1,43 @@
+class Solution {
+    public class Pair {
+        private int x, y, time;
+        public Pair(int x,int y,int time) {
+            this.x = x;
+            this.y = y;
+            this.time = time;
+        }
+    }
+    public int minTimeToReach(int[][] moveTime) {
+        int m = moveTime.length, n = moveTime[0].length;
+        int[][] dist = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            Arrays.fill(dist[i], (int)1e12);
+        }
+
+        dist[0][0] = 0;
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.time - b.time);
+
+        pq.add(new Pair(0, 0 , 0));
+        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        while(!pq.isEmpty()) {
+            Pair node = pq.poll();
+            int x = node.x, y = node.y, time = node.time;
+
+            if(dist[x][y] < time) {
+                continue;
+            }
+            for(int[] dir: dirs) {
+                int dx = dir[0] + x, dy = dir[1] + y;
+                if(dx < 0  || dx >= m || dy < 0 || dy >= n) {
+                    continue;
+                }
+                int maxTime = 1 + Math.max(moveTime[dx][dy], time);
+                if(dist[x][y] + maxTime < dist[dx][dy]) {
+                    dist[dx][dy] = maxTime;
+                    pq.add(new Pair(dx, dy, dist[dx][dy]));
+                }
+            }
+        }
+        return dist[m - 1][n - 1];
+    }
+}
